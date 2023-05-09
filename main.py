2be45a7bc4d542ed3e0e42c6482f2cfed9416784
chii-vu <chi.vu@usask.ca>
2023-05-09 16:46:32 -0600

    Wrote result to file instead of terminal

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Extract all poems
 poems = soup.find_all("div", class_=lambda x: x and x.startswith("orig_"))
 
-# Print the title, author, and poem of each poem
-for poem in poems:
-    title = poem.find_previous("h1", class_="title").text.strip()
-    author = poem.find_previous("a", class_="u nocolor").text.strip()
-    print("Title:", title)
-    print("Author:", author)
-    print("\n" + poem.text.strip() + "\n")
+# Write the output to a file
+with open("poems.txt", "w") as file:
+    # Print the title, author, and poem of each poem
+    for poem in poems:
+        title = poem.find_previous("h1", class_="title").text.strip()
+        author = poem.find_previous("a", class_="u nocolor").text.strip()
+        file.write("Title: {}\n".format(title))
+        file.write("Author: {}\n".format(author))
+        file.write("\n" + poem.text.strip() + "\n\n")

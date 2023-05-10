29fc6c3a71389be6d9ba320814bb4bfaa1ccaa50
chii-vu <chi.vu@usask.ca>
2023-05-10 16:28:33 -0600

    Added Chrome webdriver

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import time
+
+# Path to the Chrome driver executable
+driver_path = "/home/dev/chromedriver"
 
 # URL of the webpage to scrape
 url = "https://allpoetry.com/#t_picks"
 
-# Send a GET request to the URL
-response = requests.get(url)
+# Create a Chrome driver instance
+s = Service(driver_path)
+driver = webdriver.Chrome(service=s)
+
+# Load the webpage
+driver.get(url)
+
+# Wait for the page to fully load
+time.sleep(5)
+
+# Get the page source
+content = driver.page_source
 
 # Parse the HTML content using BeautifulSoup
-soup = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(content, "html.parser")
 
 # Extract all poems
 poems = soup.find_all("div", class_=lambda x: x and x.startswith("orig_"))
@@ -21,4 +39,8 @@ with open("poems.txt", "w") as file:
         author = poem.find_previous("a", class_="u nocolor").text.strip()
         file.write("Title: {}\n".format(title))
         file.write("Author: {}\n".format(author))
-        file.write("\n" + poem.text.strip() + "\n\n")
+        file.write(
+            "\n"
+            + poem.text.strip()
+            + "\n------------------------------------------------------------\n\n"
+        )
